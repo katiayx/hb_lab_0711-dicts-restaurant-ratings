db4cac3f9c7214a8a41e809c3e0e780124a46a33
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 18:21:51 -0700

    unpacking and sorting using .items method

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -8,17 +8,21 @@ for line in the_file:
     line = line.rstrip()
     line = line.split(":")
     restaurant = line[0]
-    ratings = line[1]
-    restaurant_ratings[restaurant] = ratings
-    print "{} is rated at {}".format(restaurant,ratings)
+    rating = line[1]
+    restaurant_ratings[restaurant] = rating
+    print "{} is rated at {}".format(restaurant,rating)
 
 new_restaurant = raw_input("What's your favorite restaurant?")
 new_rating = int(raw_input("How would you rate it on a scale of 1-5?"))
 restaurant_ratings[new_restaurant] = new_rating
+# sorted(restaurant_ratings.items())
+# print restaurant_ratings.items()
+
 
 # print sorted(restaurant_ratings)
-for restaurant in sorted(restaurant_ratings):
-    print restaurant, restaurant_ratings[restaurant]
+# for restaurant in sorted(restaurant_ratings):
+#     print restaurant, restaurant_ratings[restaurant]
 
-
+for restaurant, rating in restaurant_ratings.items():
+    print sorted(restaurant_ratings.items())
 
